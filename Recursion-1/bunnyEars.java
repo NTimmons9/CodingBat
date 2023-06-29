7da1e12815ca354db00918205560e62913af6769
NTimmons9 <63173256+NTimmons9@users.noreply.github.com>
2023-06-29 13:49:35 -0500

    Delete Recursion-1 directory

--- Recursion-1/bunnyEars.java
+++
@@ -1,6 +0,0 @@
-public int bunnyEars(int bunnies) {
-  if(bunnies == 0){
-    return 0;
-  }
-  return 2 + bunnyEars(bunnies -1);
-}
--- Recursion-1/sumDigits.java
+++
@@ -1,6 +0,0 @@
-public int sumDigits(int n) {
-  if (n == 0){
-    return 0;
-  }
-  return n % 10 + sumDigits(n / 10);
-}
